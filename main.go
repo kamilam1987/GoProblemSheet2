af804cbc11d4fb47db9ebff70755b2d6118da272
Kamila Michel <kamilam1987@gmail.com>
2017-10-21 11:00:53 +0100

    Add a guess route

--- main.go
+++ main.go
@@ -11,10 +11,14 @@ func main() {
 	//code adapted from:https://stackoverflow.com/questions/26559557/how-do-you-serve-a-static-html-file-using-a-go-web-server
 	// FileServe serves index.html from src folder
 	//http.Handle function tells the http package to handle all requests
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./src/index.html")
 	})
-
+	//http.Handle function tells the http package to handle all requests
+	http.HandleFunc("/guess/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./src/guess.html")
+	})
 	http.ListenAndServe(":8080", nil)
 
 }
